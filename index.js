9bbcc8717ad83e35a2ac4e6d7d19fa9fcd7908bc
Jose Ignacio <joseignacio.fernandez@gmail.com>
2020-02-12 21:18:15 +0100

    Remove babel-plugin-dotenv dependency

--- index.js
+++ index.js
@@ -1,10 +1,54 @@
-var path = require('path');
+var dotEnv = require('dotenv');
+var fs = require('fs');
+var sysPath = require('path');
+var process = require('process');
+
+function babelPluginDotenv(data) {
+    var t = data.types;
+
+    return {
+        visitor: {
+            ImportDeclaration: function(path, state) {
+                var options = state.opts;
+
+                if (options.replacedModuleName === undefined)
+                  return;
+
+                var configDir = options.configDir ? options.configDir : './';
+                var configFile = options.filename ? options.filename : '.env';
+
+                if (path.node.source.value === options.replacedModuleName) {
+                  var config = dotEnv.config({ path: sysPath.join(configDir, configFile), silent: true }) || {};
+                  var platformPath = (process.env.BABEL_ENV === 'development' || process.env.BABEL_ENV === undefined)
+                                          ? configFile + '.development'
+                                          : configFile + '.production';
+                  var config = Object.assign(config, dotEnv.config({ path: sysPath.join(configDir, platformPath), silent: true }));
+
+                  path.node.specifiers.forEach(function(specifier, idx){
+                    if (specifier.type === "ImportDefaultSpecifier") {
+                      throw path.get('specifiers')[idx].buildCodeFrameError('Import dotenv as default is not supported.')
+                    }
+                    var importedId = specifier.imported.name
+                    var localId = specifier.local.name;
+                    var binding = path.scope.getBinding(localId);
+                    binding.referencePaths.forEach(function(refPath){
+                      refPath.replaceWith(t.valueToNode(config[importedId]))
+                    });
+                  })
+
+                  path.remove();
+                }
+            }
+        }
+    }
+}
+
 
 module.exports = () => ({
   plugins: [
-    [require('babel-plugin-dotenv'), {
+    [babelPluginDotenv, {
       replacedModuleName: 'react-native-dotenv',
-      configDir: path.resolve(__dirname, "../../")
+      configDir: sysPath.resolve(__dirname, "../../")
     }],
   ],
 });
